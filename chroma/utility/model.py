7cfa208a9c86e5cc9613b88ca762e0467b83b6d7
adamnik <71239163+adamnik@users.noreply.github.com>
2025-08-02 23:09:15 -0500

    changed weights_only=False

--- chroma/utility/model.py
+++ chroma/utility/model.py
@@ -106,7 +106,7 @@ def load_model(
         )
 
     # load model weights
-    params = torch.load(weights, map_location="cpu")
+    params = torch.load(weights, map_location="cpu", weights_only=False)
     model = model_class(**params["init_kwargs"]).to(device)
     missing_keys, unexpected_keys = model.load_state_dict(
         params["model_state_dict"], strict=strict
